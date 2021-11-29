0dab298502d8ace10998967ce63a3085500ee0ef
PixiePilot <89778100+PixiePilot@users.noreply.github.com>
2021-11-29 14:27:26 +0100

    Add files via upload

---
+++ trollbridge.py
@@ -0,0 +1,12 @@
+import time
+def dialog():
+    print('group: Hey guys, does something about this bridge seem off to you too?\nIt definitely looks like someone\n lives under this bridge, I wonder who?')
+    time.sleep(3)
+    print('The troll: It is I, The troll of the bridge\n You wont be able to enter unless you solve my riddles\n but be wary, They\'re not easy\n and if you fail them, It will cost you money!')
+    time.sleep(3)
+    print('The troll: The first riddle will be:\n I\'m boring but I also take long, What am I?')
+    time.sleep(5)
+    print('group: A boardgame?')
+    time.sleep(3)
+    print('Correct!\nI suppose, I\'ll have to let you guys pass now.')
+dialog()
