055ea72b446110f221b86f2d946a1275fe04f903
PixiePilot <89778100+PixiePilot@users.noreply.github.com>
2021-11-29 14:50:04 +0100

    Add files via upload

--- treasure.py
+++ treasure.py
@@ -1,14 +1,20 @@
 import random
 def treasurechest():
-    coppercoins = random.randint(1,169420)
-    silvercoins = random.randint(1,10000)
-    silvercoins = silvercoins / 10
-    platinumcoins = random.randint(1,1000)
-    platinumcoins = platinumcoins * 24
-    goldcoins = random.randint(1,1000)
-    goldtotal = goldcoins + platinumcoins + silvercoins
-    goldtotal = float(round(goldtotal, 3))
-    return goldtotal
+    platinumcoins = 27
+    platinumcoins = platinumcoins *24
+    kroon = 93
+    robijn = 8.1
+    ring = 46.5
+    armband = 2426.4
+    question1 = 63.3
+    question2 = 17.6
+    question3 = 1.5
+
+    total = kroon + platinumcoins + robijn + ring + armband + question1 + question2 + question3
+    return total
 
 chest = treasurechest()
 print(chest,"Gold coins is in the chest!!")
+
+chest = chest / 5
+
